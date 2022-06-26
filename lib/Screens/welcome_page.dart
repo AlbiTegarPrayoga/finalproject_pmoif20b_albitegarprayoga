b1bb35b22cb7d21d9afcc7104604349b23a96834
NaufalTQ66 <naufaltaufiq66@gmail.com>
2022-06-26 11:10:21 +0700

    test

---
+++ lib/Screens/welcome_page.dart
@@ -0,0 +1,80 @@
+// import 'package:finalproject_pmoif20b_albitegarprayoga/Screens/login_page.dart';
+// import 'package:finalproject_pmoif20b_albitegarprayoga/Screens/background.dart';
+// import 'package:finalproject_pmoif20b_albitegarprayoga/Screens/register_page.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Background(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                  "assets/images/logofarming.png",
+                  height: size.height * 0.35
+              ),
+              SizedBox(height: 30),
+              Text(
+                "Pilih Login\njika sudah memiliki akun",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                height: size.height * 0.055,
+                width: size.width * 0.7,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(7),
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    color: Colors.indigoAccent[700],
+                    onPressed: () {
+                      Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => LoginPage(),),);},
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Pilih Register\njika belum memiliki akun",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                height: size.height * 0.055,
+                width: size.width * 0.7,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(7),
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    color: Colors.indigoAccent[700],
+                    onPressed: () {
+                      Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => RegisterPage(),),);},
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,22 @@
-import 'package:finalproject_pmoif20b_albitegarprayoga/register_page.dart;
+import 'package:finalproject_pmoif20b_albitegarprayoga/Screens/welcome_page.dart';
+import 'package:finalproject_pmoif20b_albitegarprayoga/constants.dart';
 import 'package:flutter/material.dart';
-import 'login_page.dart';
-import 'home_page.dart';
-
 
 void main() => runApp(MyApp());
+String username = '';
 
 class MyApp extends StatelessWidget {
-
-  final routes = <String, WidgetBuilder>{
-    LoginPage.tag: (context) => LoginPage(),
-    RegisterPage.tag: (context) => RegisterPage(),
-    HomePage.tag: (context) => HomePage(),
-    BuahPage.tag: (context) => BuahPage(),
-    SayurPage.tag:(context) => SayurPage(),
-    TanamanlainPage.tag:(context) => TanamanlainPage()
-  };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'COBACOBA',
+      title: 'Farming Tutorial',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: LoginPage(),
-      routes: routes,
+      home: WelcomePage(),
     );
   }
 }
